fff7d47e005a7e6fd88a2f22bacae41e54d7c555
Suraj Mali <surajsmali@coep.sveri.ac.in>
2025-02-26 20:47:11 +0530

    This is the HCF Code in java

---
+++ PrimeNum.java
@@ -0,0 +1,21 @@
+package Loop;
+
+import java.util.Scanner;
+
+public class PrimeNum {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        System.out.print("Enter the Any Number : ");
+        int X  = Sc.nextInt();
+        boolean flag = true;
+        for (int i = 2 ; i < X ; i++ ){
+             if ( X % i == 0 ){
+                  flag = false;
+                  break;
+             }
+        }
+
+        if ( flag == true ) System.out.println("This is Prime Number!");
+        else System.out.println("This is not Prime");
+    }
+}
