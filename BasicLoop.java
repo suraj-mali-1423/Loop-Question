e350a5797fcf46f6ddffead68e60e5c161c62b29
suraj-mali-1423 <surajsmali@coep.sveri.ac.in>
2025-02-27 01:46:52 +0530

    Implimentation of the loop

---
+++ BasicLoop.java
@@ -0,0 +1,21 @@
+package Loop;
+
+public class BasicLoop {
+    public static void main(String[] args) {
+
+
+        // implementation of the for loop
+        for (int i = 0; i < 100; i++) {
+
+        }
+
+        // Implimentation of the while loop
+
+        while( 3 < 7 ) {
+
+        }
+
+
+
+    }
+}
